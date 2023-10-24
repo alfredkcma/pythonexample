6d776f675c74c05b618e68a2f93b4e187f85fb7a
alfred <alfredkcma@gmail.com>
2023-10-25 01:32:00 +0800

    Update main.py
    exercise

--- example2/main.py
+++ example2/main.py
@@ -11,13 +11,13 @@ with open('1299.HK.csv') as f:
     csvr = csv.reader(f)
     alldata = [line for line in csvr]
     alldata.pop(0)
-    buy_and_hold = [math.log(float(alldata[i+1][5]) / float(alldata[i][5])) for i in range(len(alldata) - 1)]
+    buy_and_hold = []
 
 with open('1299.HK_signal.csv') as f:
     csvr = csv.reader(f)
     alldata = [line for line in csvr]
     alldata.pop(0)
-    buy_signal = [math.log(float(alldata[i+1][1]) / float(alldata[i][1])) for i in range(len(alldata) - 1) if int(alldata[i][4]) == 1]
+    buy_signal = []
 
 
 print(stats.ttest_ind(buy_and_hold, buy_signal, equal_var=True))
